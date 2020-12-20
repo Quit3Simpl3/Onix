cedcf4104ce8459291cfdc5d9f16787de04eddb5
Quit3Simpl3 <21604833+Quit3Simpl3@users.noreply.github.com>
2020-12-20 17:02:04 +0200

    Update onix.py

--- onix.py
+++ onix.py
@@ -1,32 +1,37 @@
-#Imports:
 from random import randint
 import json
 
-#Constants:
+# Constants:
 MAXLENGTH = 32
 MINLENGTH = 8
 
-#Translations:
-toDigits = {'l': 1, 'z':2, 'e':3, 'q':4, 's':5, 'g':6, 'g':9, 'o':0}
-toSign = {'i':'!', 'a':'@', 'h':'#', 's':'$', 'r':'&'}
+# Translations:
+to_digits = {'l': 1, 'z': 2, 'e': 3, 'q': 4, 's': 5, 'g': 9, 'o': 0}
+to_sign = {'i':'!', 'a':'@', 'h':'#', 's':'$', 'r':'&'}
 
 class Onix:
-    #Functions:
-    def __init__(self, length="8", plaintext="", options="123", banned=""):
+    def __init__(self, length=8, plaintext="", options="123", banned=""):
+        try:
+            open("dictionary.txt", "r")
+        except FileNotFoundError:
+            raise FileNotFoundError("The required dictionary.txt file was not found.")
+
+        self.localFuncs = []
+        self.cyphertext = ""
         plaintext = plaintext.lower()
         banned = banned.lower()
         
-        self.transFuncs = [self.getCapital, self.getDigit, self.getSign, self.getSmall]
+        self.transFuncs = [self.get_capital, self.get_digit, self.get_sign, self.get_small]
         self.banned = banned.split(" ")
 
         if len(plaintext) == 0:
             if int(length) < 8:
-                return "ERROR: Length is lower than 8."
+                raise Exception("ERROR: Length is lower than 8.")
             elif int(length) > 32:
-                return "ERROR: Length is higher than 32."
+                raise Exception("ERROR: Length is higher than 32.")
             else:
                 self.length = int(length)
-                self.plaintext = self.fromDic(self.length)
+                self.plaintext = self.from_dict(self.length)
         else:
             self.length = len(plaintext)
             self.plaintext = plaintext
@@ -36,71 +41,61 @@ class Onix:
         else:
             self.options = options
 
-        return None
-
-
-    def jsonOut(self):
-        jsonDictionary = {'plaintext':self.plaintext, 'length':self.length, 'cyphertext':self.toPass(self.plaintext, self.options)}
-        return json.dumps(jsonDictionary)
-
+    def json_out(self):
+        return json.dumps({
+            'plaintext':self.plaintext,
+            'length':self.length,
+            'cyphertext':self.to_password(self.plaintext, self.options)
+        })
 
     def onix(self):
-        return self.jsonOut()
+        return self.json_out()
 
-
-    def fromDic(self, length):
+    def from_dict(self, length):
         if (length > MAXLENGTH) or (length < MINLENGTH):
-            return "error"
-        else:        
-            words = open("dictionary.txt", "rb").read().split(' ')
+            raise Exception("The length isn't valid.")
 
+        else:
+            words = open("dictionary.txt", "rb").read().split(' ')
             while True:
                 word = words[randint(0, (len(words)-1))]
                 if len(word) == length:
                     return word
 
-
     #Translation Functions:
-    def getCapital(self, letter):
+    def get_capital(self, letter):
         if letter.upper() in self.banned:
             return letter
         else:
             return letter.upper()
 
-
-    def getDigit(self, letter):
+    def get_digit(self, letter):
         try:
-            if str(toDigits[letter]) in self.banned:
+            if str(to_digits[letter]) in self.banned:
                 return letter
             else:
-                return str(toDigits[letter])
+                return str(to_digits[letter])
+
         except KeyError:
             return letter
 
-
-    def getSign(self, letter):
+    def get_sign(self, letter):
         try:
-            if toSign[letter] in self.banned:
+            if to_sign[letter] in self.banned:
                 return letter
             else:
-                return toSign[letter]
+                return to_sign[letter]
         except KeyError:
             return letter
 
-    def getSmall(self, letter):
+    def get_small(self, letter):
         return letter
     
 
-    def toPass(self, plaintext, options):
-        self.cyphertext = ""
-        self.localFuncs = []
-
-        if "1" in options:
-            self.localFuncs.append(self.transFuncs[0])
-        if "2" in options:
-            self.localFuncs.append(self.transFuncs[1])
-        if "3" in options:
-            self.localFuncs.append(self.transFuncs[2])
+    def to_password(self, plaintext, options):
+        for i in range(1, 4): # Check if options include 1, 2, or 3
+            if str(i) in options:
+                self.localFuncs.append(self.transFuncs[i-1])
 
         self.localFuncs.append(self.transFuncs[3])
 
